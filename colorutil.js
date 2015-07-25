97879d579260ea3c4ac874d75620d0d155d6c19e
Steve Domenico <stratcat944@gmail.com>
2015-07-25 11:25:57 -0600

    and again

--- colorutil.js
+++ colorutil.js
@@ -1,7 +1,14 @@
-darken = function (red, blue, green){
-	for (var i = 0; i < lighten.length; i++) {
-		var percentage = lighten[i] * 0.2;
-		lighten[i] = lighten[i] - percentage;
-	}
-	return red + " " + green + " " + blue + " ";
-};
+module.exports = {
+
+	luminosity: function(red, green, blue){
+		return 0.2126*red + 0.7152*green + 0.0722*blue;
+
+		darken = function (red, blue, green){
+			for (var i = 0; i < lighten.length; i++) {
+				var percentage = lighten[i] * 0.2;
+				lighten[i] = lighten[i] - percentage;
+			}
+			return red + " " + green + " " + blue + " ";
+		};
+	};
+}
