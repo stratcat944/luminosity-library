c75a9816bf227729791dec7cb9fdae8fd6acde2a
Steve Domenico <stratcat944@gmail.com>
2015-07-25 11:08:32 -0600

    test

--- colorutil.js
+++ colorutil.js
@@ -1,3 +1,7 @@
 darken = function (red, blue, green){
-	for (var i = 0; i < lighten.length; i++)
+	for (var i = 0; i < lighten.length; i++) {
+		var percentage = lighten[i] * 0.2;
+		lighten[i] = lighten[i] - percentage;
+	}
+	return red + " " + green + " " + blue + " ";
 };
